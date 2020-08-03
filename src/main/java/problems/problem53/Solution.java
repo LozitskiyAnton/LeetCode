5f560d4245942826bcec9a1a3322b688370a92fa
Fox2k <fox2k@protonmail.com>
2020-08-03 17:10:50 +0300

    solution 53 (O(n))

--- src/main/java/problems/problem53/Solution.java
+++ src/main/java/problems/problem53/Solution.java
@@ -22,10 +22,28 @@ class Solution {
 
     }
 
+    public int maxSubArray2(int[] nums) {
+        //  int [] numbers = nums;
+        int max = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = Math.max(nums[i - 1] + nums[i], nums[i]);
+            if (nums[i] > max) {
+                max = nums[i];
+            }
+//        for (int i = 0; i <nums.length ; i++) {
+//            if(nums[i]>max){
+//                max = nums[i];
+//            }
+        }
+        return max;
+    }
+
+//
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] n= {-2,1,-3,4,-1,2,1,-5,4};
 
-        System.out.println(solution.maxSubArray(n));
+        System.out.println(solution.maxSubArray2(n));
     }
 }
