9d97952041f3a30389a5a5652382784173f45089
Fox2k <fox2k@protonmail.com>
2020-08-04 16:25:21 +0300

    solution 66

---
+++ src/main/java/problems/problem66/Solution.java
@@ -0,0 +1,28 @@
+package main.java.problems.problem66;
+
+public class Solution {
+
+    public int[] plusOne(int[] digits) {
+        if (digits[digits.length - 1] != 9) {
+            digits[digits.length - 1]++;
+            return digits;
+        }
+        else {
+            for (int i = digits.length - 1; i >= 0; i--) {
+                if (digits[i] != 9) {
+                    digits[i]++;
+                    return digits;
+                }
+                digits[i] = 0;
+            }
+            digits = new int[digits.length+1];
+            digits[0]=1;
+
+            return digits;
+
+        }
+
+    }
+
+}
+
