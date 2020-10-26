9e7606bfe1a08b69d407e84ae5138655fe28da40
Fox2k <fox2k@protonmail.com>
2020-10-26 17:01:02 +0300

    problem206_ReverseLinkedList

---
+++ src/main/java/problems/problem206_ReverseLinkedList/Solution.java
@@ -0,0 +1,26 @@
+package main.java.problems.problem206_ReverseLinkedList;
+
+
+ // Definition for singly-linked list.
+  class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+class Solution {
+    public ListNode reverseListIterative(ListNode head) {
+        ListNode next = null;
+        ListNode current = head;
+        ListNode prev = null;
+        while(current != null){
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+}
